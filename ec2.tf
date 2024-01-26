0d97d7c4c4e42a2f2f8a4a467b9f0e05abdcd9c5
kz.li <kz.li@hashicorp.com>
2024-01-26 14:06:58 +0800

    Added instance connect ip

--- ec2.tf
+++ ec2.tf
@@ -30,10 +30,10 @@ resource "aws_security_group_rule" "http_from_all" {
 resource "aws_security_group_rule" "ssh_from_ic" {
   type              = "ingress"
   description       = "AWS Instance Connect IP Address (ap-southeast-1)"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 22
+  to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["3.0.5.32/29"]
   security_group_id = data.aws_security_group.default.id
 }
 
