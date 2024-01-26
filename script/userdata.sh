3bc1bacc06e7bf45805e9dd1b5031a72fcd407a4
kz.li <kz.li@hashicorp.com>
2024-01-26 14:00:18 +0800

    Updated userdata script for instance

--- script/userdata.sh
+++ script/userdata.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+echo "Initializing instance..."
+
+# Update dependencies
 sudo yum update -y
 sudo yum install httpd -y
+
+# Download HTTP
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --reload
 
+# Create a static page
 sudo chmod 777 /var/www/html
 cat <<EOT >> /var/www/html/index.html
 <!DOCTYPE html>
@@ -13,8 +18,8 @@ cat <<EOT >> /var/www/html/index.html
     <title>Terraform Demo Web Server</title>
   </head>
   <body>
-    <h1>Apache Web Server</h1>
-    <p>This is a simple HTML web page.</p>
+    <title>Terraform Demo Web Server</title>
+    <p>This is deployment 1</p>
   </body>
 </html>
 EOT
