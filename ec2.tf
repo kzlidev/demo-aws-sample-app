b64341dbabdac8bd8ee4b9af19980ab7d2205247
kz.li <kz.li@hashicorp.com>
2024-01-26 13:39:47 +0800

    Updated AMI

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ data "aws_ami" "al2" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.3.20240122.0-kernel-6.1-arm64"]
+    values = ["al2023-ami-2023.3.20240122.0-kernel-6.1-x86_64"]
   }
 
   filter {
