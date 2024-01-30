86f49e81320d4c89017c44edfa79642b8a8a649a
kz.li <kz.li@hashicorp.com>
2024-01-30 10:35:50 +0800

    Updated checks

--- check.tf
+++ check.tf
@@ -1,12 +1,12 @@
 check "ami_version_check" {
-  data "aws_instance" "hashiapp_current" {
+  data "aws_instance" "hashicat_current" {
     instance_tags = {
-      Name = var.instance_name
+      Name = "${var.prefix}-cat-instance"
     }
   }
 
   assert {
-    condition     = aws_instance.aml2.ami == data.hcp_packer_image.custom_ami.cloud_image_id
+    condition     = data.aws_instance.hashicat_current.ami == data.hcp_packer_image.custom_ami.cloud_image_id
     error_message = "Must use the latest available AMI, ${data.hcp_packer_image.custom_ami.cloud_image_id}."
   }
 }
