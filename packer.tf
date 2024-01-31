d773d15111a12e66a728cfc5dbe51e70d9dbc0b8
kz.li <kz.li@hashicorp.com>
2024-01-31 10:47:08 +0800

    Updated deprecated packer resources

--- packer.tf
+++ packer.tf
@@ -1,11 +1,11 @@
-data "hcp_packer_iteration" "custom_ami_iteration" {
-  bucket_name = "custom-aml-2"
-  channel     = "latest"
-}
+#data "hcp_packer_version" "ami_version" {
+#  bucket_name = "custom-aml-2"
+#  channel     = "latest"
+#}
 
-data "hcp_packer_image" "custom_ami" {
+data "hcp_packer_artifact" "custom_ami" {
   bucket_name    = "custom-aml-2"
   cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.custom_ami_iteration.ulid
+  channel_name   = "latest"
   region         = var.region
 }
